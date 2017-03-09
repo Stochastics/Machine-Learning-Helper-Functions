55f157f083c6dea816c67ad1f644077e28345bc2
Bradley Strong <strong.bradley@gmail.com>
2017-03-08 19:50:03 -0600

    second update
    Some error controls added and an example of how to use

--- Feature Engineering/Loo.R
+++ Feature Engineering/Loo.R
@@ -9,7 +9,9 @@ options(stringsAsFactors = F)
 #must specify the encoded feature by string
 #also must specify random noise added by a string, for example 'runif(nrow(df),0,)'
 
-LOOencoding = function(df,feature.name,noise.str){
+LOOencoding = function(df,feature.name,noise.str="runif(nrow(df),.98,1.04)"){
+  stopifnot(sum(names(df) %in% c('target','fold_id'))>1)
+  stopifnot(is.numeric(df$target) )
   
   for(f in unique(df$fold_id)){    
     temp =  select(df,matches(feature.name),target,fold_id)
@@ -25,7 +27,7 @@ LOOencoding = function(df,feature.name,noise.str){
     
     df[[paste(feature.name,"loo",f,sep="_")]] = 
       eval(parse(text=noise.str))*df$target.rate
-
+    
     df[[paste(feature.name,"loo",f,sep="_")]] = 
       ifelse(df$fold_id==f,mean.rate,df[[paste(feature.name,"loo",f,sep="_")]])
     
@@ -34,3 +36,14 @@ LOOencoding = function(df,feature.name,noise.str){
   }
   return(df)
 }
+
+
+
+#Example how to use
+
+df = cbind.data.frame(c=rnorm(50000),a = sample(c("a","b","c"),50000,T),target=rnorm(50000))
+df$fold_id = sample(1:4,nrow(df),T)
+out= LOOencoding(df,"a","runif(nrow(df),.98,1.05)")
+
+
+
